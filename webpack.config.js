f22967cb576b762bb1b87bf6f4a54242938c4634
Gabriel Malveira <gabrielmalveira01@gmail.com>
2025-01-09 22:42:35 -0300

    vercel

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
-module.exports = {
-    entry: './src/index.js',
-    output: {
-      path: path.resolve(__dirname, 'frontend'),
-      filename: 'bundle.js'
-    },
+const path = require('path'); // Importação do módulo 'path'
 
-  };
-  
+module.exports = {
+  entry: './src/index.js',  // Arquivo de entrada
+  output: {
+    filename: 'bundle.js',  // Nome do arquivo gerado
+    path: path.resolve(__dirname, 'dist')  // Diretório de saída
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};
